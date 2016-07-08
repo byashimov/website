9537366730707cfcf50ef71e1f6d056a1e1ef694
Murad Byashimov <byashimov@Murads-MacBook-Pro.local>
2016-07-09 00:15:06 +0300

    Use class-based view for typus form page. Use typus `splinter` helper to split escape phrases

--- apps/typus_web/__init__.py
+++ apps/typus_web/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from .views import form_view
+from .views import FormView
 
 bp = Blueprint('typus_web', __name__, 'static', template_folder='templates')
-bp.add_url_rule('/', view_func=form_view, methods=('GET', 'POST'))
+bp.add_url_rule('/', view_func=FormView.as_view('form_view'))
--- apps/typus_web/views.py
+++ apps/typus_web/views.py
@@ -1,24 +1,40 @@
 import re
 
-from flask import g, render_template
+from flask import g, make_response, render_template
+from flask.views import MethodView
 from typus import en_typus, ru_typus
+from typus.utils import splinter
 
 from .forms import TypusForm
 
 phrases_delim = re.compile(r'(?<!\\),\s*')
-typus_choices = {
-    'en': en_typus,
-    'ru': ru_typus
-}
 
 
-def form_view():
-    form = TypusForm()
-    if form.validate_on_submit():
-        text = form.text.data
-        debug = form.debug.data
-        escape_phrases = phrases_delim.split(form.escape_phrases.data)
-        typus = typus_choices[g.locale]
-        form.text.data = typus(text, debug=debug,
-                               escape_phrases=escape_phrases)
-    return render_template('form_view.html', form=form)
+class FormView(MethodView):
+    method = ('GET', 'POST')
+    form_class = TypusForm
+    template_name = 'form_view.html'
+    split_phrases = staticmethod(splinter(','))
+    typus = {
+        'en': en_typus,
+        'ru': ru_typus
+    }
+
+    def get(self):
+        return self.response(form=self.form_class())
+
+    def post(self):
+        form = self.form_class()
+        if not form.validate_on_submit():
+            return self.response(422, form=form)
+
+        typus = self.typus[g.locale]
+        escape_phrases = self.split_phrases(form.escape_phrases.data)
+        form.text.data = typus(form.text.data,
+                               escape_phrases=escape_phrases,
+                               debug=form.debug.data)
+        return self.response(form=form)
+
+    def response(self, *args, **context):
+        content = render_template(self.template_name, **context)
+        return make_response(content, *args)
