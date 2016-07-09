d4d2b215d841d16f1009bb36a39cfc6b88980d28
Murad Byashimov <byashimov@Murads-MacBook-Pro.local>
2016-07-09 15:10:07 +0300

    Add command line helpers

---
+++ run.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+__all__ = {'server', 'makemessages', 'compilemessages'}
+
+
+def server():
+    """
+    Runs developer server on 0.0.0.0 in debug mode.
+    """
+
+    from website import site
+    site.run(host='0.0.0.0')
+
+
+def makemessages():
+    """
+    Makes translation messages.
+    """
+
+    os.system('pybabel extract -F babel.cfg -k lazy_gettext -o messages.pot . '
+              '&& pybabel update -i messages.pot -d translations')
+    os.unlink('messages.pot')
+
+
+def compilemessages():
+    """
+    Compiles translation messages.
+    """
+    os.system('pybabel compile -d translations')
+
+
+if __name__ == '__main__':
+    # Resolves relative imports
+    sys.path.append(os.pardir)
+
+    # Caches for help output
+    scope = globals()
+
+    # Returns help if command not on the list
+    args = sys.argv[1:]
+    if not args or args[0] not in __all__:
+        ljust = len(max(__all__, key=len))
+        descr = ('{} - {}'.format(fn.ljust(ljust), scope[fn].__doc__.strip())
+                 for fn in __all__)
+        sys.exit('Available commands:\n' + '\n'.join(descr))
+
+    # Runs command
+    scope[args[0]]()
