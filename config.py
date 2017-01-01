b036a798d01beed3c10ceb2362ec762f8f86c19c
Murad Byashimov <byashimov@gmail.com>
2017-01-02 01:26:03 +0300

    Improve site config

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = 'ru'
     BABEL_SUPPORTED_LOCALES = {'en', 'ru'}
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = True
 
 
 class Prod(Config):
     SECRET_KEY = os.getenv('SITE_SECRET_KEY')
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_DOMAIN = 'byashimov.com'
 
 
 class Dev(Config):
