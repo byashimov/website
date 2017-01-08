46097677a32c824510d86e7aebbc8b2e65b03f85
Murad Byashimov <byashimov@gmail.com>
2017-01-09 00:11:37 +0300

    Make `makemessages` strip out meta data

--- run.py
+++ run.py
@@ -37,9 +37,34 @@ def makemessages():
     Makes translation messages.
     """
 
-    os.system('pybabel extract -F babel.cfg -k lazy_gettext -o messages.pot . '
-              '&& pybabel update -i messages.pot -d translations')
-    os.unlink('messages.pot')
+    pot = os.path.join(ROOT_DIR, 'messages.pot')
+    translations = os.path.join(ROOT_DIR, 'translations')
+    babel = os.path.join(ROOT_DIR, 'babel.cfg')
+
+    # Creates translations
+    os.system('pybabel -q extract --omit-header -s --no-location '
+              '-k lazy_gettext -F {babel} -o {pot} {root} '
+              '&& pybabel update -i {pot} -d {translations}'
+              .format(root=ROOT_DIR, babel=babel, pot=pot,
+                      translations=translations))
+
+    # Removes pot file
+    os.unlink(pot)
+
+    # Removes meta data
+    for root, dirs, files in os.walk(translations):
+        pos = (f for f in files if f.endswith('.po'))
+        for po in pos:
+            path = os.path.join(root, po)
+            with open(path, 'r') as trans:
+                clean = (
+                    s for s in trans.readlines()
+                    # --omit-header doesn't work :(
+                    if not s.startswith(('"', '#', 'msgid ""', 'msgstr ""'))
+                )
+
+            with open(path, 'w+') as trans:
+                trans.writelines(''.join(clean).strip())
 
 
 def compilemessages():
