0745afa798236a8842007c96e91f11610c01cb36
Murad Byashimov <byashimov@gmail.com>
2017-01-16 22:05:58 +0300

    Add `typus_web` endpoints

--- apps/flatpages/__init__.py
+++ apps/flatpages/__init__.py
@@ -4,4 +4,5 @@ from .views import PageView
 
 bp = Blueprint('flatpages', __name__, 'static', template_folder='templates')
 bp.add_url_rule('/', defaults={'path': 'index'},
-                view_func=PageView.as_view('index'))
+                view_func=PageView.as_view('index_view'))
+bp.add_url_rule('/<path:path>/', view_func=PageView.as_view('page_view'))
